181971378791dae3854724b9d2d8a482efe7f4c1
JJ Kanu <jjkanu20@gmail.com>
2021-04-04 20:17:22 -0400

    Added pureJS and removed some React stuff

---
+++ pureJS/js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    // Splash Picture
+    // const splashPic = $("#splashPic")
+    // splashPic.css("background-image","url(myhero.png)")
+    // splashPic.css("height","900px")
+    // splashPic.css("background-repeat","no-repeat")
+    // splashPic.css("background-size","cover")
+    // splashPic.css("position","relative")
+});
