39222f0554f29a7cb1e733850e052ab495cf74ea
JJ Kanu <jjkanu20@gmail.com>
2021-05-01 16:25:11 -0400

    Added card overlays to splash

--- pureJS/js/scripts.js
+++ pureJS/js/scripts.js
@@ -10,21 +10,11 @@ $(document).ready(function() {
 });
 
 
-$(".project_card").on("mouseover" ,function(){
-    $(".project_card").css("background-color", "rgba(0,0,0,0.4)");
-    console.log("msoeon")
-});
-$(".project_card").on("mouseout", function(){
-    $(".project_card").css("background-color", "rgba(0,0,0,0)");
-    console.log("mouseout")
-});
-
-
 // Dropdown Slide Courtesy of Adam Macias at https://codepen.io/adammacias/pen/dozPVQ
 $('.dropdown').on('show.bs.dropdown', function(e){
     $(this).find('.dropdown-menu').first().stop(true, true).slideDown(300);
-  });
+});
   
-  $('.dropdown').on('hide.bs.dropdown', function(e){
+$('.dropdown').on('hide.bs.dropdown', function(e){
     $(this).find('.dropdown-menu').first().stop(true, true).slideUp(200);
-  });
+});
